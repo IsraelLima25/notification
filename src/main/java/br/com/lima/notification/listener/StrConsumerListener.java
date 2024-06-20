d35e43b27ff78b6733217cd595f208b31da2e1e2
Israel Lima <israel_lima_16@hotmail.com>
2024-06-20 10:48:17 -0300

    Custom listener

--- src/main/java/br/com/lima/notification/listener/StrConsumerListener.java
+++ src/main/java/br/com/lima/notification/listener/StrConsumerListener.java
@@ -1,8 +1,8 @@
 package br.com.lima.notification.listener;
 
+import br.com.lima.notification.custom.StrConsumerCustomListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +10,18 @@ public class StrConsumerListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StrConsumerListener.class);
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message){
-        LOGGER.info("Receive message {}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message){
+        LOGGER.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message){
+        LOGGER.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message){
+        LOGGER.info("HISTORY ::: Receive message {}", message);
     }
 }
